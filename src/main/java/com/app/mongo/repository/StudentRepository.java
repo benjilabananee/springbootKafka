972d8cff36afa8ffe732f485cd30f4842e1767b0
benjilabananee <157732325+benjilabananee@users.noreply.github.com>
2024-12-03 15:15:10 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/app/mongo/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.mongo.repository;
-
-import com.app.mongo.entities.StudentMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentRepository extends MongoRepository<StudentMongo,String> {
-}
